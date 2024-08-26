e336d0a380147dd8611ab2505878e878c245305a
박서영 <seokachuu@gmail.com>
2024-08-26 12:11:50 +0900

    [Fix] 모바일 버전에서 확대 못하도록 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={notoSansKr.className}>{children}</body>
     </html>
   );
